763fe8cb74950b6883aad49e3522f2c3d7094fe0
Govind Kumar Gupta <govindkumargupta082@gmail.com>
2025-02-03 22:59:40 +0530

    added Preloader

---
+++ VanillaJS/PRELOADER/app.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        const preload = document.querySelector('.preload');
+        preload.classList.add('preload-finish');
+    }, 5000); 
+});
